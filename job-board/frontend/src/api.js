4c63740cc356238d3ff945e187b5e42c5b7d8e8f
Allen Charles <allen.charles99@gmail.com>
2025-12-04 21:46:50 +0530

    Point API to deployed backend

--- job-board/frontend/src/api.js
+++ job-board/frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // your backend
+  baseURL: "https://jobboard-backend-hiyx.onrender.com/", // your backend
   withCredentials: true,
 });
 
